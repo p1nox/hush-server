d885d2c92d35a5907c9d29f3e86db34f3ef9b58c
p1nox <pinox101@gmail.com>
2016-03-20 10:57:44 -0300

    env configuration added for test and production

---
+++ config/env/production.js
@@ -0,0 +1,6 @@
+module.exports = {
+	env: 'production',
+	db: process.env.DB_URL,
+	port: process.env.PORT,
+	paper_url: process.env.PAPER_URL,
+};
---
+++ config/env/test.js
@@ -0,0 +1,6 @@
+module.exports = {
+	env: 'test',
+	db: 'mongodb://localhost/hush-test',
+	port: 3000,
+	paper_url: 'http://el-carabobeno.com/'
+};
