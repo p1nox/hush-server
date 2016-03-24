3d6b7049dde105372cc2dbf427182be351a45046
p1nox <pinox101@gmail.com>
2016-03-24 18:18:17 -0300

    remove mongo references and redis added

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var server = require('./server');
-var db = require('./config/db');
 var config = require('./config/env');
 var logger = require('./server/utils/logger');
-
-db.connect();
 
 server.listen(config.port, function() {
   logger.info('%s listening at <%s>', server.name, server.url);
