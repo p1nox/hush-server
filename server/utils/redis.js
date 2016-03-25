43f149f62427798cc16c0834ec46189cb49adae6
p1nox <pinox101@gmail.com>
2016-03-25 13:14:07 -0300

    redis and gitnore updated

---
+++ server/utils/redis.js
@@ -0,0 +1,41 @@
+var redis   = require('redis');
+var Promise = require('bluebird');
+
+var logger = require('./logger');
+
+Promise.promisifyAll(redis.RedisClient.prototype);
+Promise.promisifyAll(redis.Multi.prototype);
+
+module.exports = {
+
+  connect: function(redisConfig) {
+    var clientInfo = getClientInfo(redisConfig);
+
+    var client = redis.createClient(
+      redisConfig.port,
+      redisConfig.host
+    );
+
+    client.on('ready', function () {
+      logger.info('redis client connected: ', clientInfo);
+    });
+
+    client.on('error', function (err) {
+      logger.error('redis client error ', clientInfo, err);
+    });
+
+    client.auth(redisConfig.pass, function(err) {
+      if (err) {
+        logger.error(err);
+        throw err;
+      }
+    });
+
+    return client;
+  }
+
+};
+
+function getClientInfo(redisConfig) {
+  return redisConfig.host + ':' + redisConfig.port;
+}
