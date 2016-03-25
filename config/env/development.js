3d8135968b1120cefe2131a30cb416033b115c7d
p1nox <pinox101@gmail.com>
2016-03-25 13:07:35 -0300

    mongo config changed to redis

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,20 @@
 module.exports = {
-	env: 'development',
-	db: 'mongodb://localhost/hush-development',
-	port: 3000,
-	paper_url: 'http://el-carabobeno.com/'
+
+  env: 'development',
+
+  port: 3000,
+  paper_url: 'http://el-carabobeno.com/',
+
+  redis: {
+    // sections storage
+    // expired every 5 days
+    sections: {
+      port:       6379,
+      host:       'localhost',
+      pass:       '',
+      expiryTime: 60 * 60 * 24 * 5,
+      db:         0
+    }
+  }
+
 };
