67b2dd7436d4f5487d3a00ba5a5272c08d606351
p1nox <pinox101@gmail.com>
2016-03-27 20:06:03 -0300

    jobs changed

--- server/jobs/start.js
+++ server/jobs/start.js
@@ -18,8 +18,12 @@ jobs.start();
 
 // Refresh all sections every 6 hours
 function refreshAllSections() {
+  logger.info('SectionJobs.refreshAll first call executed');
+  SectionJobs.refreshAll();
+
+  logger.info('SectionJobs.refreshAll scheduled');
   schedule.scheduleJob('0 */6 * * *', function(){
-    logger.info('SectionJobs.refreshAll started!');
+    logger.info('SectionJobs.refreshAll scheduled started');
 
     return SectionJobs.refreshAll();
   });
