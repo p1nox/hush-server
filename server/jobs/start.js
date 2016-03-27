ecdb8357144b9684bd0047ae055e2ad87bcfcd50
p1nox <pinox101@gmail.com>
2016-03-27 17:18:19 -0300

    jobs started structure

--- server/jobs/start.js
+++ server/jobs/start.js
@@ -3,9 +3,23 @@ var schedule = require('node-schedule');
 var logger = require('../utils/logger');
 var SectionJobs = require('./section');
 
-// Refresh all sections every 6 hours
-schedule.scheduleJob('0 */6 * * *', function(){
-  logger.info('Job started!');
 
-  return SectionJobs.refreshAll();
-});
+var jobs = {
+
+  start: function() {
+    refreshAllSections();
+  }
+
+};
+
+jobs.start();
+
+
+// Refresh all sections every 6 hours
+function refreshAllSections() {
+  schedule.scheduleJob('0 */6 * * *', function(){
+    logger.info('Job started!');
+
+    return SectionJobs.refreshAll();
+  });
+}
