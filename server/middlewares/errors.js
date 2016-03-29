73e362a9dd3e2d3866460556b137da37fbad4350
p1nox <pinox101@gmail.com>
2016-03-28 22:52:49 -0300

    error handling improved

--- server/middlewares/errors.js
+++ server/middlewares/errors.js
@@ -5,8 +5,21 @@ module.exports = function(server) {
   server.on('InternalServer', function (req, res, err, cb) {
     logger.error('InternalServer', err);
 
-    err.body = 'something is wrong';
-    return cb();
+    handleUnexpectedError(res, err)
+  });
+
+  server.on('uncaughtException', function (req, res, route, err) {
+    logger.error('uncaughtException', err);
+
+    handleUnexpectedError(res, err)
   });
 
 };
+
+
+function handleUnexpectedError(res, err) {
+  return res.send(err.code || 500, {
+    code: err.code || 500,
+    description: 'Internal Server Error'
+  });
+}
